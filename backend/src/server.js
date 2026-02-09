caa8132d87f5a477f0a99a91d3528a888735c8ac
shiran.swisa <shiranswisa98@gmail.com>
2026-02-09 14:15:25 +0200

    URL change according to deploy

--- backend/src/server.js
+++ backend/src/server.js
@@ -16,7 +16,7 @@ const app = express();
 
 //app.use(cors());
 app.use(cors({
-  origin: 'http://localhost:5173', // וודאי שזה הפורט המדויק שלך
+  origin: process.env.FRONT_URL, // וודאי שזה הפורט המדויק שלך
   credentials: true
 }));
 
